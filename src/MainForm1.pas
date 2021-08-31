9e6a3a8c6784c8068ce5bc6d834eb5955afc51b1
kromster80 <kromster80@gmail.com>
2021-08-31 16:41:01 +0300

    Test

--- src/MainForm1.pas
+++ src/MainForm1.pas
@@ -1,7 +1,5 @@
 unit MainForm1;
-
 interface
-
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
