61a05794a34317bda40ac4c025b2e43a432fea99
LIFEfreedom <lifefreedom11@gmail.com>
2021-09-02 13:59:03 +0300

    fix

--- src/KPLauncher.dpr
+++ src/KPLauncher.dpr
@@ -1,15 +1,13 @@
 program KPLauncher;
-
 uses
   Vcl.Forms,
   MainForm1 in 'MainForm1.pas' {MainForm},
   UnitLauncher in 'UnitLauncher.pas';
 
 {$R *.res}
-
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  AApplication.CreateForm(TMainForm, MainForm);
-  pplication.Run;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
 end.
