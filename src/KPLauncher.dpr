92748ffe3afec36c02aa05908e57123e0bf2089e
LIFEfreedom <lifefreedom11@gmail.com>
2021-09-02 13:58:24 +0300

    save

--- src/KPLauncher.dpr
+++ src/KPLauncher.dpr
@@ -2,13 +2,14 @@ program KPLauncher;
 
 uses
   Vcl.Forms,
-  MainForm1 in 'MainForm1.pas' {MainForm};
+  MainForm1 in 'MainForm1.pas' {MainForm},
+  UnitLauncher in 'UnitLauncher.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TMainForm, MainForm);
-  Application.Run;
+  AApplication.CreateForm(TMainForm, MainForm);
+  pplication.Run;
 end.
