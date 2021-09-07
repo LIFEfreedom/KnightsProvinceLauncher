1212f5113931384c9def214bf5c8d3fd05ace6b5
kromster80 <kromster80@gmail.com>
2021-09-07 08:48:44 +0300

    Formatting Comments Fixed CRLF (again)

--- src/UnitLauncher.pas
+++ src/UnitLauncher.pas
@@ -1,97 +1,87 @@
 unit UnitLauncher;
-
 interface
-
 uses
-    Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-    System.Classes, Vcl.Graphics,
-    Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
-    Vcl.Imaging.jpeg, ShellAPI;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.jpeg, ShellAPI;
 
 type
-    TKPLauncher = class
-
-    const
-        // Mutex is used to block duplicate app launch on the same PC
-        // Random GUID generated in Delphi by Ctrl+Shift+G
-        KP_MUTEX = 'DA046FE1-82E6-45EA-B25E-2C3F2350593C';
-
-    private
-        fMutex: THandle;
-
-    public
-        function CheckDuplicate;
-        procedure CheckUpdate;
-        procedure LaunchGame;
-        procedure UnlockMutex;
-        procedure LockMutex;
-    end;
+  TKPLauncher = class
+  const
+    // Mutex is used to block duplicate app launch on the same PC
+    // Random GUID generated in Delphi by Ctrl+Shift+G
+    // @Life: Is this a KP mutex or Launcher mutex? Comment should be updated
+    KP_MUTEX = 'DA046FE1-82E6-45EA-B25E-2C3F2350593C';
+  private
+    fMutex: THandle;
+  public
+    function CheckDuplicate;
+    procedure CheckUpdate;
+    procedure LaunchGame;
+    procedure UnlockMutex;
+    procedure LockMutex;
+  end;
 
 implementation
 
+{ TKPLauncher }
 procedure TKPLauncher.LaunchGame;
 var
-    si: TStartupInfo;
-    pi: TProcessInformation;
-    shi: TShellExecuteInfo;
+  si: TStartupInfo;
+  pi: TProcessInformation;
+  shi: TShellExecuteInfo;
 begin
+  shi := Default(TShellExecuteInfo);
+  shi.cbSize := SizeOf(TShellExecuteInfo);
+  shi.lpFile := PChar('KnightsProvince.exe');
+  shi.nShow := SW_SHOWNORMAL;
 
-    shi := Default (TShellExecuteInfo);
-    shi.cbSize := SizeOf(TShellExecuteInfo);
-    shi.lpFile := PChar('KnightsProvince.exe');
-    shi.nShow := SW_SHOWNORMAL;
-
-    ShellExecuteEx(@shi);
-
+  ShellExecuteEx(@shi);
 end;
+
 
 procedure TKPLauncher.LockMutex;
 begin
-{$IFDEF MSWINDOWS}
-    // if DBG_MP_ALLOW_DOUBLE_APP then
-    // Exit;
+  // if DBG_ALLOW_DOUBLE_APP then Exit;
 
-    fMutex := CreateMutex(nil, True, PChar(KP_MUTEX));
+  fMutex := CreateMutex(nil, True, PChar(KP_MUTEX));
 
-    if fMutex = 0 then
-        RaiseLastOSError;
-{$ENDIF}
+  if fMutex = 0 then
+    RaiseLastOSError;
 end;
+
 
 procedure TKPLauncher.CheckUpdate;
 begin
-    { check update }
+  { check update }
 end;
+
 
 function TKPLauncher.CheckDuplicate: Boolean;
 begin
-{$IFDEF MSWINDOWS}
-    Result := True;
-    // if DBG_MP_ALLOW_DOUBLE_APP then
-    // Exit;
+  Result := True;
+  // if DBG_ALLOW_DOUBLE_APP then Exit;
 
-    LockMutex();
+  LockMutex;
 
-    Result := (GetLastError <> ERROR_ALREADY_EXISTS);
+  Result := (GetLastError <> ERROR_ALREADY_EXISTS);
 
-    if Result = True then
-        UnlockMutex;
-    // Close our own handle on the mutex because someone else already made the mutex
-{$ENDIF}
-{$IFDEF UNIX}
-    Result := True;
-{$ENDIF}
+  if Result = True then
+    UnlockMutex;
+  // Close our own handle on the mutex because someone else already made the mutex
 end;
+
 
 procedure TKPLauncher.UnlockMutex;
 begin
-{$IFDEF MSWINDOWS}
-    // if DBG_MP_ALLOW_DOUBLE_APP then Exit;
-    if fMutex = 0 then
-        Exit; // Didn't have a mutex lock
-    CloseHandle(fMutex);
-    fMutex := 0;
-{$ENDIF}
+  // if DBG_ALLOW_DOUBLE_APP then Exit;
+  if fMutex = 0 then
+    Exit; // Didn't have a mutex lock
+
+  CloseHandle(fMutex);
+  fMutex := 0;
 end;
+
 
 end.
